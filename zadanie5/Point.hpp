ff9392865891edfda59c4384b077ab1bedc37562
Szymon Nieznański <s.nez@member.fsf.org>
2015-01-28 19:21:50 +0100

    Add more explanation in comments

--- zadanie5/Point.hpp
+++ zadanie5/Point.hpp
@@ -8,7 +8,7 @@
 #include <cstdio>
 
 struct Point {
-	float x, y;
+	float x, y; // Coordinates
 	char label;
 
 	Point() : x (0), y (0), label ('0') {}
@@ -26,6 +26,7 @@ std::ostream& operator << (std::ostream& out, const Point& p)
 	return out << " " << p.label << ": ( " << p.x << " , " << p.y << ")";
 }
 
+// Calculate euclidean distance between two points
 float dist (const Point lhs, const Point rhs)
 {
 	float x_diff = rhs.x - lhs.x;
@@ -37,15 +38,20 @@ struct Path {
 	float length;
 	std::vector<Point> order;
 
-	Path (float dl, std::vector<Point> list) : length (dl), order (list) {};
+	Path (float nl, std::vector<Point> list) : length (nl), order (list) {};
 
+	// Return a path with x prepended at the beginning,
+	// doesn't modify the original path.
 	Path prepend (Point x)
 	{
-		Path tmp = *this;
+		Path tmp = *this; 
 		tmp.length += dist (x, tmp.order[0]);
 		tmp.order.insert (tmp.order.begin(), x);
 		return tmp;
 	}
+
+	// Return a path with x appended at the end,
+	// doesn't modify the original path.
 	Path append (Point x)
 	{
 		Path tmp = *this;
@@ -55,12 +61,12 @@ struct Path {
 	}
 };
 
-std::ostream& operator << (std::ostream& out, const Path d)
+std::ostream& operator << (std::ostream& out, const Path p)
 {
 	out << "Path: ";
-	for (auto x : d.order)
+	for (auto x : p.order)
 		out << x.label;
-	out << ", length = " << d.length;
+	out << ", length = " << p.length;
 	return out;
 }
 
@@ -70,6 +76,7 @@ inline bool same_endpoints (const Path& a, const Path& b)
 	       || (a.order[0] == b.order.back() && a.order.back() == b.order[0]);
 }
 
+// From paths with the same endpoints leave only the shortest one
 std::vector<Path> remove_suboptimal (const std::vector<Path>& list)
 {
 	std::vector<Path> tmp;
@@ -89,17 +96,21 @@ std::vector<Path> remove_suboptimal (const std::vector<Path>& list)
 	return std::move (tmp);
 }
 
+// Take a list of points sorted by their x coordinate and calculate
+// the shortest bitonic tour
 Path optimal_bitonic_tour (std::vector<Point> list)
 {
+	using std::begin;
+	using std::end;
+
 	std::vector<Path> result;
 
 	// init AB
 	result.push_back (Path (dist (list[0], list[1]), { list[0], list[1] }));
 
-	// the main algorithm
-	auto i_point = list.begin();
-	i_point += 2; // skip A and B
-	for (auto end = list.end(); i_point != end; ++i_point) {
+	// skip A and B which are already in the result vector and iterate over
+	// all the other points to find the best path
+	for (auto i_point = begin(list) + 2; i_point != end(list); ++i_point) {
 		std::vector<Path> TMP;
 		for (auto i_path : result) {
 			TMP.push_back (i_path.prepend (*i_point));
@@ -108,6 +119,7 @@ Path optimal_bitonic_tour (std::vector<Point> list)
 		result = remove_suboptimal (TMP);
 	}
 
+	// Print all closed bitonic tours 
 	//std::cout << "Mamy wyniki:" << std::endl;
 	//for (auto x : result) {
 		//for (auto y : x.order)
@@ -115,6 +127,7 @@ Path optimal_bitonic_tour (std::vector<Point> list)
 		//std::cout << " = " << x.length + dist (x.order[0], x.order.back()) << std::endl;
 	//}
 
+	// Pick the shortest tour from TMP and return it
 	return *min_element (result.begin(), result.end(),
 	[] (Path x, Path y) { return x.length < y.length; });
 }

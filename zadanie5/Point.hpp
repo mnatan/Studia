2f0b9104bf0a08e91ecdc0eb5f1b1d26ec4e0f82
Szymon Nieznański <s.nez@member.fsf.org>
2015-01-28 19:28:09 +0100

    Coding style corrections

--- zadanie5/Point.hpp
+++ zadanie5/Point.hpp
@@ -15,53 +15,53 @@ struct Point {
 	Point (float nx, float ny, char nlabel) : x (nx), y (ny), label (nlabel) {}
 };
 
-bool operator== (const Point& a, const Point& b)
+bool operator==(const Point& a, const Point& b)
 {
 	// We assume the labels are unique
 	return a.label == b.label;
 }
 
-std::ostream& operator << (std::ostream& out, const Point& p)
+std::ostream& operator<<(std::ostream& out, const Point& p)
 {
 	return out << " " << p.label << ": ( " << p.x << " , " << p.y << ")";
 }
 
 // Calculate euclidean distance between two points
-float dist (const Point lhs, const Point rhs)
+float dist(const Point lhs, const Point rhs)
 {
 	float x_diff = rhs.x - lhs.x;
 	float y_diff = rhs.y - lhs.y;
-	return sqrt (x_diff * x_diff + y_diff * y_diff);
+	return sqrt(x_diff * x_diff + y_diff * y_diff);
 }
 
 struct Path {
 	float length;
 	std::vector<Point> order;
 
-	Path (float nl, std::vector<Point> list) : length (nl), order (list) {};
+	Path(float nl, std::vector<Point> list) : length (nl), order (list) {};
 
 	// Return a path with x prepended at the beginning,
 	// doesn't modify the original path.
-	Path prepend (Point x)
+	Path prepend(Point x)
 	{
 		Path tmp = *this; 
-		tmp.length += dist (x, tmp.order[0]);
-		tmp.order.insert (tmp.order.begin(), x);
+		tmp.length += dist(x, tmp.order[0]);
+		tmp.order.insert(tmp.order.begin(), x);
 		return tmp;
 	}
 
 	// Return a path with x appended at the end,
 	// doesn't modify the original path.
-	Path append (Point x)
+	Path append(Point x)
 	{
 		Path tmp = *this;
-		tmp.length += dist (x, tmp.order.back());
-		tmp.order.push_back (x);
+		tmp.length += dist(x, tmp.order.back());
+		tmp.order.push_back(x);
 		return tmp;
 	}
 };
 
-std::ostream& operator << (std::ostream& out, const Path p)
+std::ostream& operator<<(std::ostream& out, const Path p)
 {
 	out << "Path: ";
 	for (auto x : p.order)
@@ -70,20 +70,20 @@ std::ostream& operator << (std::ostream& out, const Path p)
 	return out;
 }
 
-inline bool same_endpoints (const Path& a, const Path& b)
+inline bool same_endpoints(const Path& a, const Path& b)
 {
 	return (a.order[0] == b.order[0] && a.order.back() == b.order.back())
 	       || (a.order[0] == b.order.back() && a.order.back() == b.order[0]);
 }
 
 // From paths with the same endpoints leave only the shortest one
-std::vector<Path> remove_suboptimal (const std::vector<Path>& list)
+std::vector<Path> remove_suboptimal(const std::vector<Path>& list)
 {
 	std::vector<Path> tmp;
 	for (auto x : list) {
 		bool x_is_shortest = true;
 		for (auto y : list) {
-			if (same_endpoints (x, y) and (y.length < x.length)) {
+			if (same_endpoints(x, y) and (y.length < x.length)) {
 				x_is_shortest = false;
 				break;
 			}
@@ -91,14 +91,14 @@ std::vector<Path> remove_suboptimal (const std::vector<Path>& list)
 		// If no shorter path with the same endpoints as x was found,
 		// add it to the result
 		if (x_is_shortest)
-			tmp.push_back (x);
+			tmp.push_back(x);
 	}
-	return std::move (tmp);
+	return std::move(tmp);
 }
 
 // Take a list of points sorted by their x coordinate and calculate
 // the shortest bitonic tour
-Path optimal_bitonic_tour (std::vector<Point> list)
+Path optimal_bitonic_tour(std::vector<Point> list)
 {
 	using std::begin;
 	using std::end;
@@ -106,17 +106,17 @@ Path optimal_bitonic_tour (std::vector<Point> list)
 	std::vector<Path> result;
 
 	// init AB
-	result.push_back (Path (dist (list[0], list[1]), { list[0], list[1] }));
+	result.push_back(Path (dist (list[0], list[1]), { list[0], list[1] }));
 
 	// skip A and B which are already in the result vector and iterate over
 	// all the other points to find the best path
 	for (auto i_point = begin(list) + 2; i_point != end(list); ++i_point) {
 		std::vector<Path> TMP;
 		for (auto i_path : result) {
-			TMP.push_back (i_path.prepend (*i_point));
-			TMP.push_back (i_path.append (*i_point));
+			TMP.push_back(i_path.prepend (*i_point));
+			TMP.push_back(i_path.append (*i_point));
 		}
-		result = remove_suboptimal (TMP);
+		result = remove_suboptimal(TMP);
 	}
 
 	// Print all closed bitonic tours 
